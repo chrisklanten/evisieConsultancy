9ec1556e4158d6fe7d0f20deec003dd1cf0a908f
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2020-09-08 20:37:51 +0200

    add index fields to cms

--- src/blocks/reviews/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Wat zeggen klanten over Evisie Consultancy?
-reviews:
-  - review: Test reveiw
-    name: chris
-    job: developer
-    logo: /img/coffee-gear.png
----
--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -5,6 +5,7 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Features from "../components/Features";
 import BlogRoll from "../components/BlogRoll";
+import ReviewBlock from "../components/ReviewBlock";
 
 export const HomePageTemplate = ({
   image,
@@ -16,52 +17,20 @@ export const HomePageTemplate = ({
   intro,
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          height: "150px",
-          lineHeight: "1",
-          justifyContent: "space-around",
-          alignItems: "left",
-          flexDirection: "column",
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+    <h1>{title}</h1>
+    {/* <div className="flex">
+      <div>
+      </div>
+      <div>
+        <div
           style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em",
+            backgroundImage: `url(${
+              !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+            backgroundPosition: `top left`,
+            backgroundAttachment: `fixed`,
           }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em",
-          }}
-        >
-          {subheading}
-        </h3>
+        ></div>
       </div>
     </div>
     <section className="section section--gradient">
@@ -110,7 +79,8 @@ export const HomePageTemplate = ({
           </div>
         </div>
       </div>
-    </section>
+    </section> */}
+    <ReviewBlock />
   </div>
 );
 
@@ -128,19 +98,11 @@ HomePageTemplate.propTypes = {
 
 const HomePage = ({ data }) => {
   console.log(data);
-  const { frontmatter } = data.index;
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
-      <HomePageTemplate
-        image={frontmatter.mainImage}
-        title={frontmatter.title}
-        heading={frontmatter.mainText}
-        subheading={frontmatter.cta}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
+      <HomePageTemplate title={frontmatter.title} />
     </Layout>
   );
 };
@@ -160,35 +122,63 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "home-page" } }) {
       frontmatter {
         title
-        mainImage {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        mainText
-        cta
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
 `;
+
+// export const pageQuery = graphql`
+//   query HomePageTemplate {
+//     markdownRemark(frontmatter: { templateKey: { eq: "home-page" } }) {
+//       frontmatter {
+//         intro {
+//           title
+//           mainText
+//           cta
+//           mainImage {
+//             childImageSharp {
+//               fluid(maxWidth: 2048, quality: 100) {
+//                 ...GatsbyImageSharpFluid
+//               }
+//             }
+//           }
+//         }
+//         servicesBlock {
+//           title
+//           intro
+//           services {
+//             title
+//             breadtext
+//           }
+//         }
+//         about {
+//           title
+//           mainText
+//           aboutImage {
+//             childImageSharp {
+//               fluid(maxWidth: 2048, quality: 100) {
+//                 ...GatsbyImageSharpFluid
+//               }
+//             }
+//           }
+//         }
+//         reviews {
+//           title
+//           reviews {
+//             title
+//             review
+//             name
+//             job
+//             logo {
+//               childImageSharp {
+//                 fluid(maxWidth: 240, quality: 64) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
