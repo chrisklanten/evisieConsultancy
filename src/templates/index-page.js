bf1c6dfc32f27c8cef2f8720d9afe3b579aabdde
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2020-09-13 10:51:45 +0200

    change config

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,94 +7,56 @@ import Features from "../components/Features";
 import BlogRoll from "../components/BlogRoll";
 import logo from "../img/logo.svg";
 
-export const IndexPageTemplate = ({
-	image,
-	title,
-	heading,
-	subheading,
-	mainpitch,
-	description,
-	intro,
-}) => (
-	<div className="flex justify-center items-center h-screen">
-		<img src={logo} alt="Evisie Consultancy" style={{ width: "197px" }} />
-	</div>
+export const IndexPageTemplate = () => (
+  <div className="flex justify-center items-center h-screen">
+    <img src={logo} alt="Evisie Consultancy" style={{ width: "197px" }} />
+  </div>
 );
 
-IndexPageTemplate.propTypes = {
-	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-	title: PropTypes.string,
-	heading: PropTypes.string,
-	subheading: PropTypes.string,
-	mainpitch: PropTypes.object,
-	description: PropTypes.string,
-	intro: PropTypes.shape({
-		blurbs: PropTypes.array,
-	}),
-};
+// IndexPageTemplate.propTypes = {
+// 	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+// 	title: PropTypes.string,
+// 	heading: PropTypes.string,
+// 	subheading: PropTypes.string,
+// 	mainpitch: PropTypes.object,
+// 	description: PropTypes.string,
+// 	intro: PropTypes.shape({
+// 		blurbs: PropTypes.array,
+// 	}),
+// };
 
-const IndexPage = ({ data }) => {
-	const { frontmatter } = data.markdownRemark;
+const IndexPage = () => {
+  //const { frontmatter } = data.markdownRemark;
 
-	return (
-		<Layout>
-			<IndexPageTemplate
-				image={frontmatter.image}
-				title={frontmatter.title}
-				heading={frontmatter.heading}
-				subheading={frontmatter.subheading}
-				mainpitch={frontmatter.mainpitch}
-				description={frontmatter.description}
-				intro={frontmatter.intro}
-			/>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <IndexPageTemplate
+      // image={frontmatter.image}
+      // title={frontmatter.title}
+      // heading={frontmatter.heading}
+      // subheading={frontmatter.subheading}
+      // mainpitch={frontmatter.mainpitch}
+      // description={frontmatter.description}
+      // intro={frontmatter.intro}
+      />
+    </Layout>
+  );
 };
 
 IndexPage.propTypes = {
-	data: PropTypes.shape({
-		markdownRemark: PropTypes.shape({
-			frontmatter: PropTypes.object,
-		}),
-	}),
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
 };
 
 export default IndexPage;
 
-export const pageQuery = graphql`
-	query IndexPageTemplate {
-		markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-			frontmatter {
-				title
-				image {
-					childImageSharp {
-						fluid(maxWidth: 2048, quality: 100) {
-							...GatsbyImageSharpFluid
-						}
-					}
-				}
-				heading
-				subheading
-				mainpitch {
-					title
-					description
-				}
-				description
-				intro {
-					blurbs {
-						image {
-							childImageSharp {
-								fluid(maxWidth: 240, quality: 64) {
-									...GatsbyImageSharpFluid
-								}
-							}
-						}
-						text
-					}
-					heading
-					description
-				}
-			}
-		}
-	}
-`;
+// export const pageQuery = graphql`
+//   query IndexPageTemplate {
+//     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+//       templateKey
+//     }
+//   }
+// `;
